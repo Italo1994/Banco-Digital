27a9c9ad0f60ef7b00e3ff5b7883e4bd418eeeaa
Italo1994 <italohortiz2015@gmail.com>
2022-06-16 21:34:06 -0300

    Criando os métodos para adicionar e listar as contas da classe Banco

--- src/model/Banco.java
+++ src/model/Banco.java
@@ -1,11 +1,16 @@
 package model;
 
-import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
 
 public class Banco {
+    private static int NUMERO_DE_CONTAS = 0;
     private String nome;
-    private List<Conta> contas;
+    private Set<Conta> contas;
 
+    public Banco() {
+        contas = new HashSet<Conta>();
+    }
     public void setNome(String nome) {
         this.nome = nome;
     }
@@ -13,4 +18,17 @@ public class Banco {
     public String getNome() {
         return nome;
     }
+
+    public void adicionarConta(Conta conta) {
+        contas.add(conta);
+        NUMERO_DE_CONTAS++;
+    }
+    public void imprimirContas() {
+        System.out.println(String.format("Instituição Bancária: %s", this.nome));
+        System.out.println(String.format("Quantidade de Contas Cadastradas: %d", this.NUMERO_DE_CONTAS));
+        for(Conta conta : contas) {
+            conta.imprimirExtrato();
+        }
+    }
+
 }
--- src/view/App.java
+++ src/view/App.java
@@ -6,13 +6,21 @@ public class App {
     public static void main(String[] args) {
         Cliente cl1 = new Cliente();
         cl1.setNome("Italo Hortiz do Nascimento");
+
         Conta cc = new ContaCorrente(cl1);
         Conta cp = new ContaPoupanca(cl1);
+
+        Banco banco1 = new Banco();
+        banco1.setNome("Banco do Brasil");
 
         cc.depositar(1000);
         cc.transferir(100, cp);
 
-        cc.imprimirExtrato();
-        cp.imprimirExtrato();
+        banco1.adicionarConta(cc);
+        banco1.adicionarConta(cp);
+        banco1.imprimirContas();
+
+//        cc.imprimirExtrato();
+//        cp.imprimirExtrato();
     }
 }
